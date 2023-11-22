026a7c744125254a703f9c77028c12f325fe9a91
iuri <iuri.olari@elara-gmbh.de>
2023-11-22 23:49:56 +0100

    test example

--- st_app.py
+++ st_app.py
@@ -1,4 +1,26 @@
+# Import necessary libraries
 import streamlit as st
 
-st.write("pizza2")
 
+# Define the main function for your app
+def main():
+    # Give your app a title
+    st.title("Simple Square Calculator")
+
+    # Add a sidebar with some information
+    st.sidebar.header("About")
+    st.sidebar.text("This is a simple app to calculate the square of a number.")
+
+    # Get user input for a number
+    user_input = st.number_input("Enter a number:", min_value=0)
+
+    # Calculate the square of the input number
+    square_result = user_input ** 2
+
+    # Display the result
+    st.write(f"The square of {user_input} is: {square_result}")
+
+
+# Run the app
+if __name__ == "__main__":
+    main()
